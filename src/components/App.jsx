0ba5b70659a59d3cd083e8b34d89477ef4fa66d2
Afiron09 <comixgod@gmail.com>
2024-02-01 23:41:59 +0200

    4

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,23 @@
 import { Component } from "react";
+import { ContactForm } from "./ContactForm/ContactForm.jsx";
+import { ContactsList } from "./ContactsList/ContactsList.jsx";
+import { Filter } from "./Filter/Filter.jsx";
 
-export default class App extends Component {
+export class App extends Component {
+  state = {
+    contacts: [],
+  };
+
+  handleAddContacts = (userContacts) => {
+    if (
+      this.state.contacts.some((contact) => contact.name === userContacts.name)
+    ) {
+      alert(`${userContacts.name} is already in contacts`);
+    }
+    this.setState({
+      contacts: [userContacts, ...this.state.contacts],
+    });
+  };
   render() {
     return <div>App</div>;
   }
